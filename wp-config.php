bc914191ddb3ead73066dc25e72ff6924bcbeae2
Trac Nguyen <npbtrac@yahoo.com>
2024-02-23 14:22:05 +0700

    Update with the db port

--- wp-config.php
+++ wp-config.php
@@ -19,11 +19,11 @@
  * @package WordPress
  */
 
-require_once dirname(__FILE__) . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv_loader = new josegonzalez\Dotenv\Loader( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . '.env' );
+$dotenv_loader = new josegonzalez\Dotenv\Loader( __DIR__ . DIRECTORY_SEPARATOR . '.env' );
 // Parse the .env file and send the parsed .env file to the $_ENV variable
-//	and put to getenv()
+//  and put to getenv()
 $dotenv_loader->parse()->toEnv()->putenv( true );
 
 // ** MySQL settings ** //
@@ -36,9 +36,6 @@ define( 'DB_USER', getenv( 'DB_USER' ) );
 /** MySQL database password */
 define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) );
 
-/** MySQL hostname */
-define( 'DB_HOST', getenv( 'DB_HOST' ) );
-
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
 
@@ -50,6 +47,16 @@ define( 'DB_TABLE_PREFIX', getenv( 'DB_TABLE_PREFIX' ) ? getenv( 'DB_TABLE_PREFI
 // Extra db params
 if ( getenv( 'DB_PORT' ) ) {
 	define( 'DB_PORT', getenv( 'DB_PORT' ) );
+	$db_host = getenv( 'DB_HOST' );
+	if ( $db_host && strpos( $db_host, ':' ) === false ) {
+		define( 'DB_HOST', $db_host . ':' . DB_PORT );
+	} else {
+		/** MySQL hostname */
+		define( 'DB_HOST', getenv( 'DB_HOST' ) );
+	}
+} else {
+	/** MySQL hostname */
+	define( 'DB_HOST', getenv( 'DB_HOST' ) );
 }
 
 if ( getenv( 'DB_SOCKET' ) && ! DB_HOST ) {
@@ -73,15 +80,15 @@ if ( getenv( 'DB_ENGINE' ) ) {
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY', getenv( 'AUTH_KEY' ) ?: hash( 'sha256', md5( php_uname( 'n' ).'1' ) ) );
-define( 'SECURE_AUTH_KEY', getenv( 'SECURE_AUTH_KEY' ) ?: hash( 'sha256', md5( php_uname( 'n' ).'2' ) ) );
-define( 'LOGGED_IN_KEY', getenv( 'LOGGED_IN_KEY' ) ?: hash( 'sha256', md5( php_uname( 'n' ).'3' ) ) );
-define( 'NONCE_KEY', getenv( 'NONCE_KEY' ) ?: hash( 'sha256', md5( php_uname( 'n' ).'4' ) ) );
-define( 'AUTH_SALT', getenv( 'AUTH_SALT' ) ?: hash( 'sha256', md5( php_uname( 'n' ).'5' ) ) );
-define( 'SECURE_AUTH_SALT', getenv( 'SECURE_AUTH_SALT' ) ?: hash( 'sha256', md5( php_uname( 'n' ).'6' ) ) );
-define( 'LOGGED_IN_SALT', getenv( 'LOGGED_IN_SALT' ) ?: hash( 'sha256', md5( php_uname( 'n' ).'7' ) ) );
-define( 'NONCE_SALT', getenv( 'NONCE_SALT' ) ?: hash( 'sha256', md5( php_uname( 'n' ).'8' ) ) );
-define( 'WP_CACHE_KEY_SALT', getenv( 'WP_CACHE_KEY_SALT' ) ?: hash( 'sha256', md5( php_uname( 'n' ).'9' ) ) );
+define( 'AUTH_KEY', getenv( 'AUTH_KEY' ) ?: hash( 'sha256', md5( php_uname( 'n' ) . '1' ) ) );
+define( 'SECURE_AUTH_KEY', getenv( 'SECURE_AUTH_KEY' ) ?: hash( 'sha256', md5( php_uname( 'n' ) . '2' ) ) );
+define( 'LOGGED_IN_KEY', getenv( 'LOGGED_IN_KEY' ) ?: hash( 'sha256', md5( php_uname( 'n' ) . '3' ) ) );
+define( 'NONCE_KEY', getenv( 'NONCE_KEY' ) ?: hash( 'sha256', md5( php_uname( 'n' ) . '4' ) ) );
+define( 'AUTH_SALT', getenv( 'AUTH_SALT' ) ?: hash( 'sha256', md5( php_uname( 'n' ) . '5' ) ) );
+define( 'SECURE_AUTH_SALT', getenv( 'SECURE_AUTH_SALT' ) ?: hash( 'sha256', md5( php_uname( 'n' ) . '6' ) ) );
+define( 'LOGGED_IN_SALT', getenv( 'LOGGED_IN_SALT' ) ?: hash( 'sha256', md5( php_uname( 'n' ) . '7' ) ) );
+define( 'NONCE_SALT', getenv( 'NONCE_SALT' ) ?: hash( 'sha256', md5( php_uname( 'n' ) . '8' ) ) );
+define( 'WP_CACHE_KEY_SALT', getenv( 'WP_CACHE_KEY_SALT' ) ?: hash( 'sha256', md5( php_uname( 'n' ) . '9' ) ) );
 
 /**
  * WordPress Database Table prefix.
@@ -93,31 +100,31 @@ $table_prefix = DB_TABLE_PREFIX;
 
 /* That's all, stop editing! Happy blogging. */
 define( 'WP_ENV', getenv( 'WP_ENV' ) );
-define( 'WP_DEBUG', isset($debug_override) ? $debug_override : ! ! getenv( 'WP_DEBUG' ) );
-define( 'WP_DEBUG_DISPLAY', ! ! getenv( 'WP_DEBUG_DISPLAY' )  );
+define( 'WP_DEBUG', isset( $debug_override ) ? $debug_override : ! ! getenv( 'WP_DEBUG' ) );
+define( 'WP_DEBUG_DISPLAY', ! ! getenv( 'WP_DEBUG_DISPLAY' ) );
 define( 'WP_DEBUG_LOG', ( getenv( 'WP_DEBUG_LOG' ) ? getenv( 'WP_DEBUG_LOG' ) : 1 ) ); // set to 'true' or 1 means the default debug.log file would be wp-content/debug.log
 
 define( 'SAVEQUERIES', ! ! getenv( 'SAVEQUERIES' ) );
 
 // For Multisite
 // https://wordpress.org/documentation/article/nginx/
-define( 'WP_ALLOW_MULTISITE', isset($_ENV['WP_ALLOW_MULTISITE']) ? ! ! getenv( 'WP_ALLOW_MULTISITE' ) : false );
-define( 'MULTISITE', isset($_ENV['MULTISITE']) ? ! ! getenv( 'MULTISITE' ) : false );
-define( 'SUBDOMAIN_INSTALL', isset($_ENV['SUBDOMAIN_INSTALL']) ? ! ! getenv( 'SUBDOMAIN_INSTALL' ) : false );
-define( 'DOMAIN_CURRENT_SITE', isset($_ENV['DOMAIN_CURRENT_SITE']) ? getenv( 'DOMAIN_CURRENT_SITE' ) : '' );
-define( 'PATH_CURRENT_SITE', isset($_ENV['PATH_CURRENT_SITE']) ? getenv( 'PATH_CURRENT_SITE' ) : '/' );
-define( 'SITE_ID_CURRENT_SITE', isset($_ENV['SITE_ID_CURRENT_SITE']) ? (int) getenv( 'SITE_ID_CURRENT_SITE' ) : 1 );
-define( 'BLOG_ID_CURRENT_SITE', isset($_ENV['BLOG_ID_CURRENT_SITE']) ? (int) getenv( 'BLOG_ID_CURRENT_SITE' ) : 1 );
+define( 'WP_ALLOW_MULTISITE', isset( $_ENV['WP_ALLOW_MULTISITE'] ) ? ! ! getenv( 'WP_ALLOW_MULTISITE' ) : false );
+define( 'MULTISITE', isset( $_ENV['MULTISITE'] ) ? ! ! getenv( 'MULTISITE' ) : false );
+define( 'SUBDOMAIN_INSTALL', isset( $_ENV['SUBDOMAIN_INSTALL'] ) ? ! ! getenv( 'SUBDOMAIN_INSTALL' ) : false );
+define( 'DOMAIN_CURRENT_SITE', isset( $_ENV['DOMAIN_CURRENT_SITE'] ) ? getenv( 'DOMAIN_CURRENT_SITE' ) : '' );
+define( 'PATH_CURRENT_SITE', isset( $_ENV['PATH_CURRENT_SITE'] ) ? getenv( 'PATH_CURRENT_SITE' ) : '/' );
+define( 'SITE_ID_CURRENT_SITE', isset( $_ENV['SITE_ID_CURRENT_SITE'] ) ? (int) getenv( 'SITE_ID_CURRENT_SITE' ) : 1 );
+define( 'BLOG_ID_CURRENT_SITE', isset( $_ENV['BLOG_ID_CURRENT_SITE'] ) ? (int) getenv( 'BLOG_ID_CURRENT_SITE' ) : 1 );
 
 // Important on using different domain
 // The domain structure should be: a domain for the main site of the network and sub-domains for the sub-site or other domains for the sub site e.g.:
-//	- demo.dev-srv.net for the main site
+//  - demo.dev-srv.net for the main site
 //  - sub1.demo.dev-srv.net sub2.demo.dev-srv.net ... for the sub sites
-//	- or abc.com, xyz.dev ... for the sub sites, this time, the cookie domain
-//		should be set to that domain
-if (MULTISITE) {
-	$current_domain = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '';
-	if ($current_domain and strpos($current_domain, DOMAIN_CURRENT_SITE) === false) {
+//  - or abc.com, xyz.dev ... for the sub sites, this time, the cookie domain
+//      should be set to that domain
+if ( MULTISITE ) {
+	$current_domain = isset( $_SERVER['HTTP_HOST'] ) ? $_SERVER['HTTP_HOST'] : '';
+	if ( $current_domain and strpos( $current_domain, DOMAIN_CURRENT_SITE ) === false ) {
 		define( 'COOKIE_DOMAIN', $current_domain );
 	}
 }
@@ -145,11 +152,11 @@ define( 'ENPII_BASE_WP_APP_BASE_PATH', getenv( 'ENPII_BASE_WP_APP_BASE_PATH' ) ?
 define( 'WP_APP_TELESCOPE_ENABLED', ! ! getenv( 'WP_APP_TELESCOPE_ENABLED' ) );
 define( 'WP_APP_TINKER_ENABLED', ! ! getenv( 'WP_APP_TINKER_ENABLED' ) );
 define( 'WP_APP_PASSPORT_ENABLED', ! ! getenv( 'WP_APP_PASSPORT_ENABLED' ) );
-define( 'ARTISAN_BINARY', isset($_ENV['ARTISAN_BINARY']) ? (string) getenv( 'ARTISAN_BINARY' ) : 'artisan' );
+define( 'ARTISAN_BINARY', isset( $_ENV['ARTISAN_BINARY'] ) ? (string) getenv( 'ARTISAN_BINARY' ) : 'artisan' );
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+	define( 'ABSPATH', __DIR__ . '/' );
 }
 
 /** Sets up WordPress vars and included files. */
